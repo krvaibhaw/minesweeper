08674736736784d1a039619f89e3b14b32ce8fe8
Vaibhaw <kvaibhaw99@gmail.com>
2020-10-11 11:15:00 -0400

    Updating minesweeper.py

--- minesweeper.py
+++ minesweeper.py
@@ -17,50 +17,68 @@ import random
 
 
 class Minesweeper():
+    
+
     """
     Minesweeper game representation
+    
     """
 
     def __init__(self, height=8, width=8, mines=8):
 
+        
         # Set initial width, height, and number of mines
+        
         self.height = height
         self.width = width
         self.mines = set()
 
         # Initialize an empty field with no mines
+        
         self.board = []
+        
         for i in range(self.height):
             row = []
+            
             for j in range(self.width):
                 row.append(False)
             self.board.append(row)
 
         # Add mines randomly
+        
         while len(self.mines) != mines:
+            
             i = random.randrange(height)
             j = random.randrange(width)
+            
             if not self.board[i][j]:
                 self.mines.add((i, j))
                 self.board[i][j] = True
 
         # At first, player has found no mines
+        
         self.mines_found = set()
 
     def print(self):
+        
         """
         Prints a text-based representation
         of where mines are located.
+        
         """
         
         for i in range(self.height):
             print("--" * self.width + "-")
+            
             for j in range(self.width):
+                
                 if self.board[i][j]:
                     print("|X", end="")
+                
                 else:
                     print("| ", end="")
             print("|")
+        
         print("--" * self.width + "-")
 
     
