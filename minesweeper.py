1b75c46ca8e0597fb4a6223f7d52da076b44c515
Vaibhaw <kvaibhaw99@gmail.com>
2020-10-16 11:15:00 -0400

    Updating minesweeper.py

--- minesweeper.py
+++ minesweeper.py
@@ -119,10 +119,12 @@ class Minesweeper():
 
 
 class Sentence():
+    
     """
     Logical statement about a Minesweeper game
     A sentence consists of a set of board cells,
     and a count of the number of those cells which are mines.
+    
     """
 
     def __init__(self, cells, count):
@@ -136,25 +138,31 @@ class Sentence():
         return f"{self.cells} = {self.count}"
 
     def known_mines(self):
+        
         """
         Returns the set of all cells in self.cells known to be mines.
+        
         """
         
         if len(self.cells) == self.count:
             return self.cells
 
     def known_safes(self):
+        
         """
         Returns the set of all cells in self.cells known to be safe.
+        
         """
         
         if self.count == 0:
             return self.cells
 
     def mark_mine(self, cell):
+        
         """
         Updates internal knowledge representation given the fact that
         a cell is known to be a mine.
+        
         """
         
         if cell in self.cells:
@@ -162,9 +170,11 @@ class Sentence():
             self.count -= 1
 
     def mark_safe(self, cell):
+        
         """
         Updates internal knowledge representation given the fact that
         a cell is known to be safe.
+        
         """
         
         if cell in self.cells:
@@ -172,41 +182,56 @@ class Sentence():
  
 
 class MinesweeperAI():
+   
     """
     Minesweeper game player
+    
     """
 
     def __init__(self, height=8, width=8):
 
         # Set initial height and width
+        
         self.height = height
         self.width = width
 
         # Keep track of which cells have been clicked on
+        
         self.moves_made = set()
 
         # Keep track of cells known to be safe or mines
+        
         self.mines = set()
         self.safes = set()
 
         # List of sentences about the game known to be true
+        
         self.knowledge = []
 
+
     def mark_mine(self, cell):
+        
         """
         Marks a cell as a mine, and updates all knowledge
         to mark that cell as a mine as well.
+        
         """
+        
         self.mines.add(cell)
+        
         for sentence in self.knowledge:
             sentence.mark_mine(cell)
 
     def mark_safe(self, cell):
+        
         """
         Marks a cell as safe, and updates all knowledge
         to mark that cell as safe as well.
+        
         """
+        
         self.safes.add(cell)
+        
         for sentence in self.knowledge:
             sentence.mark_safe(cell)
 
