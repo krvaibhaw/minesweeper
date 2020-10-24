4b70da59cf68f6222b8574386b2df37e071f9cdd
Vaibhaw <kvaibhaw99@gmail.com>
2020-10-24 11:15:00 -0400

    Updating runner.py

--- runner.py
+++ runner.py
@@ -25,7 +25,7 @@ screen = pygame.display.set_mode(size)
 
 # Fonts
 
-OPEN_SANS = "assets/font/OpenSans-Regular.ttf"
+OPEN_SANS = "assets/fonts/OpenSans-Regular.ttf"
 smallFont = pygame.font.Font(OPEN_SANS, 20)
 mediumFont = pygame.font.Font(OPEN_SANS, 28)
 largeFont = pygame.font.Font(OPEN_SANS, 40)
